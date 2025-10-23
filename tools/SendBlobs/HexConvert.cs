7184d18f83f6dd1bc3809055b093f5448e8a046b
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-10-23 11:15:18 +0300

    Update HexConvert.cs

--- tools/SendBlobs/HexConvert.cs
+++ tools/SendBlobs/HexConvert.cs
@@ -4,14 +4,14 @@
 using Nethermind.Int256;
 
 namespace SendBlobs;
-internal static class HexConvert
+internal static class RpcQuantityParser
 {
-    public static UInt256 ToUInt256(string s)
+    public static UInt256 ParseUInt256(string s)
     {
-        return (UInt256)ToUInt64(s);
+        return (UInt256)ParseUInt64(s);
     }
 
-    public static ulong ToUInt64(string s)
+    public static ulong ParseUInt64(string s)
     {
         return Convert.ToUInt64(s, s.StartsWith("0x") ? 16 : 10);
     }
